4cca97d82ec32fa28bcd2c13d72c7938e79dbba3
SerhiiVoshta <dellnames@gmail.com>
2022-08-03 23:05:36 +0300

    title html

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -72,7 +72,7 @@ export default function Gallery() {
                     <Link className='gallery-download-link' to="/reference_KMKSTROY.pdf" target="_blank" download>
                         <img src={process.env.PUBLIC_URL + '/img/gallery_pdf_icon.svg'} alt=".pdf" height="30" />
                         {t('gallery.download')} (pdf)
-                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#3f3f3f"><g><rect fill="none" height="24" width="24" /></g><g><path d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z" /></g></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#3f3f3f"><g><rect fill="none" height="24" width="24" /></g><g><path d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z" /></g></svg>
                     </Link>
                     <Box sx={{ width: 1, minHeight: 'auto', overflow: 'hidden' }}>
                         <Masonry sx={{ width: 'auto' }} columns={{ xs: 1, sm: 2, md: 3 }} spacing={{ xs: 1, sm: 2, md: 3 }}>
